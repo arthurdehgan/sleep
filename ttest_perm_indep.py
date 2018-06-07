0ebe257170ada7fb52ded80c3c2d74505807b02d
arthurdehgan <arthurdehgan@github.com>
2018-06-07 15:30:22 -0400

    updates and adding multifeature

--- ttest_perm_indep.py
+++ ttest_perm_indep.py
@@ -3,10 +3,13 @@
 Author: Arthur Dehgan"""
 import numpy as np
 from scipy.stats import ttest_ind
+from scipy.special import comb
+from itertools import combinations
+from joblib import Parallel, delayed
 
 
-def ttest_perm_ind_maxcor(cond1, cond2, n_perm=0, correction='maxstat'
-                          equal_var=False, two_tailed=True):
+def ttest_perm_unpaired(cond1, cond2, n_perm=0, correction=None,
+                        equal_var=False, two_tailed=True, n_jobs=1):
     """ttest indep with permuattions and maxstat correction
 
     Parameters:
@@ -15,30 +18,54 @@ def ttest_perm_ind_maxcor(cond1, cond2, n_perm=0, correction='maxstat'
                       the independant conditions.
 
         n_perm: int, 0, number of permutations to do.
-               If n_perm = 0 then exaustive permutations will be done.
-               It will take exponential time with data size.
+                If n_perm = 0 then exaustive permutations will be done.
+                It will take exponential time with data size.
+
+        correction: string, None, the choice of correction to compute
+                    pvalues. If None, no correction will be done
+                    Options are 'maxstat', 'FDR', None
 
         equal_var: bool, False, see scipy.stats.ttest_ind.
 
         two_tailed: bool, False, if you want two-tailed ttest.
 
+        n_jobs: int, 1, Number of cores used to computer permutations in
+                parallel
+
     Returns:
         tval: list, the calculated t-statistics
 
-        p_val: list if two_tailed = False
+        pval: list if two_tailed = False
                tuple(list: pval_right, list: pval_left) otherwisei
                pvalues after permutation test
     """
     tval = ttest_ind(cond1, cond2, equal_var=equal_var)[0]
 
-    perm_t = perm_test(cond1, cond2, n_perm, equal_var)
+    perm_t = perm_test(cond1, cond2, n_perm, equal_var, n_jobs=n_jobs)
 
-    pval = compute_maxstat_pval(tval, perm_t, two_tailed)
+    pval = compute_pvalues(tval, perm_t, two_tailed)
 
     return tval, pval
 
 
-def perm_test(cond1, cond2, n_perm, equal_var):
+def my_combinations(iterable, r, limit=None):
+    i = 0
+    for e in combinations(iterable, r):
+        yield e
+        i += 1
+        if limit is not None and i == limit:
+            break
+
+
+def do_ttest_perm(data, index, equal_var):
+    index = list(index)
+    index_comp = list(set(range(len(data))) - set(index))
+    perm_mat = np.vstack((data[index], data[index_comp]))
+    cond1, cond2 = perm_mat[:len(index)], perm_mat[len(index):]
+    return ttest_ind(cond1, cond2, equal_var=equal_var)[0]
+
+
+def perm_test(cond1, cond2, n_perm, equal_var, n_jobs):
     """permuattion ttest.
 
     Parameters:
@@ -54,21 +81,29 @@ def perm_test(cond1, cond2, n_perm, equal_var):
         perm_t: list of permutation t-statistics
     """
     full_mat = np.concatenate((cond1, cond2), axis=0)
+    n_samples = len(full_mat)
     perm_t = []
-    if n_perm == 0:
-        # for id in combinations(range(len(cond1) + len(cond2)), min(len(cond1), len(cond2))):
-            # perm_t.append(ttest_ind(cond1[
-    else:
-        for _ in range(n_perm):
-            np.random.shuffle(full_mat)
-            cond1 = full_mat[:len(cond1)]
-            cond2 = full_mat[len(cond2):]
-            perm_t.append(ttest_ind(cond1, cond2, equal_var=equal_var)[0])
-    return perm_t
+    n_comb = int(comb(n_samples, len(cond1)))
+
+    if n_perm == 'all' or n_perm >= n_comb - 1:
+        # print("All permutations will be done. n_perm={}".format(n_comb - 1))
+        n_perm = n_comb
+    if n_perm > 9999:
+        print('Warning: permutation number is very high : {}'.format(n_perm))
+        print('it might take a while to compute ttest on all permutations')
+    # else:
+        # print("{} permutations will be done".format(n_perm))
+
+    perms_index = my_combinations(range(n_samples), len(cond1), n_perm)
+    perm_t = Parallel(n_jobs=n_jobs)(delayed(do_ttest_perm)
+                                     (full_mat, index, equal_var)
+                                     for index in perms_index)
+
+    return perm_t[1:]  # the first perm is not a permutation
 
 
-def compute_maxstat_pval(tval, perm_t, two_tailed):
-    """computes pvalues with maxstat correction.
+def compute_pvalues(tval, perm_t, two_tailed, correction=None):
+    """computes pvalues without any correction.
 
     Parameters:
         tstat: computed t-statistics
@@ -79,39 +114,27 @@ def compute_maxstat_pval(tval, perm_t, two_tailed):
 
     Returns:
         pvalues: list if two_tailed = False
-                 tuple(list: pval_right, list: pval_left) otherwise
                  pvalues after permutation test
     """
-    pvalues_right = []
-    max_t_perm = np.asarray(perm_t).max(axis=1)
+    scaling = len(perm_t)
+    perm_t = np.array(perm_t)
+    pvalues = []
     if two_tailed:
-        pvalues_left = []
-        min_t_perm = np.asarray(perm_t).min(axis=1)
+        perm_t = abs(perm_t)
 
-    for tstat in tval:
-        p_final_right = 0
-        for t_perm in max_t_perm:
-            if tstat >= t_perm:
-                p_final_right += 1/(len(perm_t)+1)
-        pvalues_right.append(float('{:.3f}'.format(p_final_right)))
+    if correction == 'maxstat':
+        perm_t = np.asarray(perm_t).max(axis=1)
+    elif correction == None:
+        continue
+    else:
+        raise('This correction option has not been implemented yes')
 
-        if two_tailed:
-            p_final_left = 0
-            for t_perm in min_t_perm:
-                if tstat <= t_perm:
-                    p_final_left += 1/(len(perm_t)+1)
-            pvalues_left.append(float('{:.3f}'.format(p_final_left)))
-
-    pvalues = (pvalues_left, pvalues_right) if two_tailed\
-        else pvalues_right
+    for i, tstat in enumerate(tval):
+        p_final = 0
+        compare_list = perm_t if correction == 'maxstat' else perm_t[:, i]
+        for t_perm in compare_list:
+            if tstat <= t_perm:
+                p_final += 1/scaling
+        pvalues.append(p_final)
 
     return pvalues
-
-
-if __name__ == '__main__':
-    cond1 = np.random.random((10, 10))
-    cond2 = np.random.random((10, 10))
-    print(cond1)
-    print(cond2)
-    n_perm = 10
-    print(ttest_perm_ind_maxcor(cond1, cond2, n_perm))
