921d68050691c79b24bd3441b6dc90eb69ddd7c6
arthurdehgan <arthurdehgan@gmail.com>
2018-09-17 11:37:41 -0400

    start

--- classif_cov.py
+++ classif_cov.py
@@ -1,6 +1,6 @@
 """Load covariance matrix, perform classif, perm test, saves results.
 
-Outputs one file per freq x state
+Outputs one file per freq x cond
 
 Author: Arthur Dehgan"""
 from time import time
@@ -16,80 +16,47 @@ from utils import (
     prepare_data,
     classification,
 )
-from params import SAVE_PATH, STATE_LIST, LABEL_PATH
 
-# import pdb
-
-# name = 'moy_cov'
-prefix = "classif_subsamp_"
+prefix = "classif_"
 name = "cov"
 
+SAVE_PATH = ""
+COND_LIST = ""
+LABEL_PATH = ""
 pref_list = prefix.split("_")
-BOOTSTRAP = "bootstrapped" in pref_list
-FULL_TRIAL = "ft" in pref_list or "moy" in pref_list
-SUBSAMPLE = "subsamp" in pref_list
 PERM = "perm" in pref_list
 N_PERM = 999 if PERM else None
-N_BOOTSTRAPS = 10 if BOOTSTRAP else 1
 
 SAVE_PATH = SAVE_PATH / name
 
 
-def main(state):
+def main(cond):
     """Where the magic happens"""
-    print(state)
-    if FULL_TRIAL:
-        labels = np.concatenate((np.ones(18), np.zeros(18)))
-        groups = range(36)
-    elif SUBSAMPLE:
-        info_data = pd.read_csv(SAVE_PATH.parent / "info_data.csv")[STATE_LIST]
-        N_TRIALS = info_data.min().min()
-        N_SUBS = len(info_data) - 1
-        groups = [i for i in range(N_SUBS) for _ in range(N_TRIALS)]
-        N_TOTAL = N_TRIALS * N_SUBS
-        labels = [0 if i < N_TOTAL / 2 else 1 for i in range(N_TOTAL)]
-    else:
-        labels = loadmat(LABEL_PATH / state + "_labels.mat")["y"].ravel()
-        labels, groups = create_groups(labels)
+    print(cond)
+    labels = loadmat(LABEL_PATH / cond + "_labels.mat")["y"].ravel()
+    labels, groups = create_groups(labels)
 
-    file_name = prefix + name + "_{}.mat".format(state)
+    file_name = prefix + name + "_{}.mat".format(cond)
 
     save_file_path = SAVE_PATH / "results" / file_name
 
     if not save_file_path.isfile():
-        data_file_path = SAVE_PATH / name + "_{}.mat".format(state)
+        data_file_path = SAVE_PATH / name + "_{}.mat".format(cond)
 
         if data_file_path.isfile():
-            final_save = None
+            data = loadmat(data_file_path)
+            data = prepare_data(data)
 
-            for i in range(N_BOOTSTRAPS):
-                data = loadmat(data_file_path)
-                if FULL_TRIAL:
-                    data = data["data"]
-                elif SUBSAMPLE:
-                    data = prepare_data(data, n_trials=N_TRIALS, random_state=i)
-                else:
-                    data = prepare_data(data)
+            sl2go = StratifiedLeave2GroupsOut()
+            lda = LDA()
+            clf = TSclassifier(clf=lda)
+            save = classification(clf, sl2go, data, labels, groups, N_PERM, n_jobs=-1)
 
-                sl2go = StratifiedLeave2GroupsOut()
-                lda = LDA()
-                clf = TSclassifier(clf=lda)
-                save = classification(
-                    clf, sl2go, data, labels, groups, N_PERM, n_jobs=-1
-                )
-                save["acc_bootstrap"] = [save["acc_score"]]
-                save["auc_bootstrap"] = [save["auc_score"]]
-                if final_save is None:
-                    final_save = save
-                else:
-                    for key, value in final_save.items():
-                        final_save[key] = final_save[key] + save[key]
-
-            savemat(save_file_path, final_save)
+            savemat(save_file_path, save)
 
             print(
                 "accuracy for %s : %0.2f (+/- %0.2f)"
-                % (state, save["acc_score"], np.std(save["acc"]))
+                % (cond, save["acc_score"], np.std(save["acc"]))
             )
 
         else:
@@ -98,6 +65,6 @@ def main(state):
 
 if __name__ == "__main__":
     TIMELAPSE_START = time()
-    for state in STATE_LIST:
-        main(state)
+    for cond in COND_LIST:
+        main(cond)
     print("total time lapsed : %s" % elapsed_time(TIMELAPSE_START, time()))
